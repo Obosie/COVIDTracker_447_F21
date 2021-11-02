dcfb0cb91da9069ca096fcfecb93f4322741cd90
Brian Scherzo <44170955+BBScherz@users.noreply.github.com>
2021-11-02 13:47:22 -0400

    Application Layer
    The first push to the application layer of the project.  Can query valid dates for California and respond with confirmed resident cases (can be changed)

---
+++ app.js
@@ -0,0 +1,79 @@
+const { query } = require('express');
+const express = require('express');
+const http = require('https');
+const fs = require('fs');
+const sql = require('mysql');
+const { createServer } = require('http');
+
+
+const pkey = fs.readFileSync(__dirname + "\\certs\\server.key", {encoding:'utf-8'});
+const crt = fs.readFileSync(__dirname + "\\certs\\server.cert", {encoding:'utf-8'});
+var creds = {key: pkey, cert: crt};
+
+
+var req_out; 
+
+function query_pull(date){
+
+    // -------- Retrieval Query --------
+    console.log("Attempting retrieval query...");
+    let retrieve = "SELECT * FROM historical_state_counts WHERE(State = 'California' AND Date = '" + date + "')";
+    database.query(retrieve, (err,result) => {
+ 
+        if(err){
+            console.log("Error retrieving object\nObject does not exist");
+            throw err;
+        }
+
+        var ret = JSON.stringify(result[0]);
+    
+        req_out = ret;
+    })
+}
+
+
+
+
+
+
+const app = express();
+var server = createServer(creds, app);
+
+server.listen('3000', () => {
+    
+    console.log("Server started on port 3000");
+});
+app.get('/', (req, result) => {
+
+    result.send("HTTPS Connection Established...");
+});
+app.get('/get/:year-:month-:day', (req, result) => {
+
+        let p = req.params;
+        let date = p.year + "-" + p.month + "-" + p.day;
+        
+        query_pull(date);
+        let json_out = JSON.parse(req_out);
+        
+        result.send("200 OK");
+        console.log("On " + date + ": Residents Positive = " + json_out["Residents.Confirmed"]);
+});
+
+
+
+
+
+const database = sql.createConnection({
+
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'covid_19_prison_tracker'
+});
+database.connect((err) => {
+
+    if(err) throw err;
+    console.log("Connected to COVID19 mySQL...");
+});
+
+
