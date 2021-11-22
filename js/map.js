ab5600a8b0851cd117a88b66e18245a7bf1d5233
Brian Scherzo <44170955+BBScherz@users.noreply.github.com>
2021-11-22 16:58:15 -0500

    Map Javascript Updates
    Map functionality updates

--- js/map.js
+++ js/map.js
@@ -1,9 +1,39 @@
 
-var mapboxAccessToken = "pk.eyJ1IjoiYnNjaGVyeiIsImEiOiJja3ZvNzQ3MG5lNHN3MnFtYW1keWYyMnAwIn0.hN-2JumWaHsnPsBqyz9etQ";
-var map = L.map('map').setView([37.8, -96], 4);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + mapboxAccessToken, {
-            id: 'mapbox/light-v9',
-            tileSize: 512,
-            zoomOffset: -1
-}).addTo(map);
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+
+    $('#user_in').submit((e) => {
+    
+        e.preventDefault();
+        
+        var fdata = $('#userdate').val();
+        var action = e.currentTarget.action;
+        
+        $.ajax({
+            type: "POST",
+            url: "/get-data",
+            async: false,
+            data: {udate: fdata},
+            dataType: "json",
+            success: function(data){
+
+                console.log("Captured data!")
+            },
+            error: function(jqXHR, textStatus, err){
+
+                alert("Status: " + textStatus + ", Error: " + err);
+            }
+           
+        });
+    });
+
+});
