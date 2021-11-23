f79ce926813e27c1d861da8d607a53c9e8cce049
Brian Scherzo <44170955+BBScherz@users.noreply.github.com>
2021-11-23 17:01:17 -0500

    Prison Rendering
    Now able to add markers to the map for valid prison data and redraw the map without reloading the whole webpage

--- js/map.js
+++ js/map.js
@@ -2,6 +2,31 @@
 
 
 
+function update(data){
+
+    for(let e = 0; e < data.length; e++){
+
+        let obj = data[e];
+        if(obj.Address == null && obj.Jurisdiction == null){
+
+            console.log("Object at ",e," is county information");
+        }else{
+
+            let name = obj.Name;
+            let lat = obj.Latitude;
+            let long = obj.Longitude;
+
+            var circle = L.circle([lat,long], {
+                color: 'grey',
+                fillColor: '#03b1fc',
+                fillOpacity: 0.4,
+                radius: 5000
+            }).addTo(map);
+
+            circle.bindPopup(name);
+        }
+    }
+}
 
 
 
@@ -26,7 +51,8 @@ $(document).ready(function() {
             dataType: "json",
             success: function(data){
 
-                console.log("Captured data!")
+                update(data);
+                map.redraw();
             },
             error: function(jqXHR, textStatus, err){
 
